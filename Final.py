8e1d8cc553514bd495f2cf1ea5d6187036cf262b
kpage1 <48829396+kpage1@users.noreply.github.com>
2020-04-20 07:50:50 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -8,6 +8,22 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/")
+def players():
+    return render_template("players.html")
+
+
+@app.route("/")
+def gameData():
+    return render_template("gameData.html")
+
+@app.route("/")
+def shopping():
+    return render_template("shopping.html")
+
+@app.route("/")
+def teams():
+    return render_template("teams.html")
 
 #@app.route("/<name>")
 #def user(name):
